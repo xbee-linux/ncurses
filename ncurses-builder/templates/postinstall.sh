941d410dbbe8689dee99b9afde61bcba59de12f6
Eric FISCHER <efischer78@yahoo.fr>
2022-03-13 09:19:37 +0100

    ok

--- ncurses-builder/templates/postinstall.sh
+++ ncurses-builder/templates/postinstall.sh
@@ -10,6 +10,7 @@ done
 echo "INPUT(-lncursesw)" > {{ .pack.xbeeOut }}/usr/lib/libcursesw.so
 ln -sfv libncurses.so      {{ .pack.xbeeOut }}/usr/lib/libcurses.so
 
+mv {{ .pack.xbeeOut }}/usr/lib/terminfo {{ .pack.xbeeOut }}/usr/lib/terminfo2
 #documentation
 #mkdir -pv       {{ .pack.xbeeOut }}/usr/share/doc/ncurses-6.2
 #cp -v -R doc/* {{ .pack.xbeeOut }}/usr/share/doc/ncurses-6.2
