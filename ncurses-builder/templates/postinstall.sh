5d074d27eb4840f567f22a8b8225584a4603691b
Eric Fischer <efischer78@yahoo.fr>
2023-07-31 21:33:53 +0200

    ok

--- ncurses-builder/templates/postinstall.sh
+++ ncurses-builder/templates/postinstall.sh
@@ -2,15 +2,15 @@
 set -e
 
 for lib in ncurses form panel menu ; do
-    rm -vf                    {{ .xbeeOut }}/usr/lib/lib${lib}.so
-    echo "INPUT(-l${lib}w)" > {{ .xbeeOut }}/usr/lib/lib${lib}.so
-    ln -sfv ${lib}w.pc        {{ .xbeeOut }}/usr/lib/pkgconfig/${lib}.pc
+    rm -vf                    {{ .xbee.out }}/usr/lib/lib${lib}.so
+    echo "INPUT(-l${lib}w)" > {{ .xbee.out }}/usr/lib/lib${lib}.so
+    ln -sfv ${lib}w.pc        {{ .xbee.out }}/usr/lib/pkgconfig/${lib}.pc
 done
 
-echo "INPUT(-lncursesw)" > {{ .xbeeOut }}/usr/lib/libcursesw.so
-ln -sfv libncurses.so      {{ .xbeeOut }}/usr/lib/libcurses.so
+echo "INPUT(-lncursesw)" > {{ .xbee.out }}/usr/lib/libcursesw.so
+ln -sfv libncurses.so      {{ .xbee.out }}/usr/lib/libcurses.so
 
-mv {{ .xbeeOut }}/usr/lib/terminfo {{ .xbeeOut }}/usr/lib/terminfo2
+mv {{ .xbee.out }}/usr/lib/terminfo {{ .xbee.out }}/usr/lib/terminfo2
 #documentation
-#mkdir -pv       {{ .xbeeOut }}/usr/share/doc/ncurses-6.2
-#cp -v -R doc/* {{ .xbeeOut }}/usr/share/doc/ncurses-6.2
+#mkdir -pv       {{ .xbee.out }}/usr/share/doc/ncurses-6.2
+#cp -v -R doc/* {{ .xbee.out }}/usr/share/doc/ncurses-6.2
